77d53b25ef6eb080845b1dac8337cac0be6b9114
Michael Weimann <mail@michael-weimann.eu>
2022-06-07 11:52:56 +0200

    Fix plugin URL in migration notice

--- lua/telescope/_extensions/live_grep_raw.lua
+++ lua/telescope/_extensions/live_grep_raw.lua
@@ -5,8 +5,8 @@
 
 local migration = [[
 "live_grep_raw" has been renamed to "live_grep_args":
-- Change the plugin source to "nvim-telescope/telescope-live_grep_args.nvim"
-  (new Git repo: https://github.com/nvim-telescope/telescope-live_grep_args.nvim)
+- Change the plugin source to "nvim-telescope/telescope-live-grep-args.nvim"
+  (new Git repo: https://github.com/nvim-telescope/telescope-live-grep-args.nvim)
 - Replace all occurrences of "live_grep_raw" with "live_grep_args" in your config
 ]]
 vim.notify(migration, vim.log.levels.WARN)
