5117f43fa8de07e5d4697480a9cea6b8ecf7813a
lytex <julianlpc15@gmail.com>
2023-01-22 00:15:15 +0100

    implement tokenization

--- lua/telescope-live-grep-args/actions/tokenize.lua
+++ lua/telescope-live-grep-args/actions/tokenize.lua
@@ -6,18 +6,56 @@ local default_opts = {
 	trim = true,
 }
 
+
 return function(opts)
 	opts = opts or {}
 	opts = vim.tbl_extend("force", default_opts, opts)
 
 	return function(prompt_bufnr)
+
+		local wrap, yield = coroutine.wrap, coroutine.yield
+
+		local function permgen(a, n)
+				n = n or #a
+				if n <= 1 then
+						yield(a)
+				else
+						for i = 1, n do
+								-- put i-th element as the last one
+								a[n], a[i] = a[i], a[n]
+								-- generate all permutations of the other elements
+								permgen(a, n - 1)
+								-- restore i-th element
+								a[n], a[i] = a[i], a[n]
+						end
+				end
+		end
+
+		function permutations(a)
+				-- call with coroutine.wrap()
+				return wrap(function() permgen(a) end)
+		end
+
 		local picker = action_state.get_current_picker(prompt_bufnr)
 		local prompt = picker:_get_prompt()
 		if opts.trim then
 			prompt = vim.trim(prompt)
 		end
-		prompt = prompt:gsub(opts.quote_char, "\\" .. opts.quote_char)
-		prompt = opts.quote_char .. prompt .. opts.quote_char .. opts.postfix
+		local tokens = {}
+		-- TODO interpret two spaces or more as a literal space
+		for token in prompt:gmatch("%S+") do
+			table.insert(tokens, token)
+		end
+
+		prompt = ""
+		-- TODO Remove duplicate permutations
+		-- a a b → a.*b.*a|b.*a.*a|b.*a.*a|a.*b.*a|a.*a.*b|a.*b.*a it should be:
+		-- a a b → a.*b.*a|b.*a.*a|a.*a.*b
+		for combo in permutations(tokens) do
+			prompt = prompt .. "|" .. table.concat(combo, '.*')
+		end
+		prompt = prompt:sub(2)
+
 		picker:set_prompt(prompt)
 	end
 end
