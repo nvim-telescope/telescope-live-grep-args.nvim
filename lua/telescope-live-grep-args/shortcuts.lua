2371c42287b6dd7ae868542f03b6fd0407250aed
ajenkinski <adam@thejenkins.org>
2023-08-28 08:42:50 -0400

    Fix get_visual argument order bug
    Ensure that the `start_row`, `start_col`, `end_row`, and `end_col` arguments to `nvim_buf_get_text`
    are passed in the correct order regardless of the direction of the visual selection.  Previously,
    if the end of the visual selection was before the start, `nvim_buf_get_text` would raise an error
    saying "start_col must be less than end_col".

--- lua/telescope-live-grep-args/shortcuts.lua
+++ lua/telescope-live-grep-args/shortcuts.lua
@@ -8,6 +8,11 @@ local helpers = require("telescope-live-grep-args.helpers")
 local function get_visual()
   local _, ls, cs = unpack(vim.fn.getpos("v"))
   local _, le, ce = unpack(vim.fn.getpos("."))
+
+  -- nvim_buf_get_text requires start and end args be in correct order
+  ls, le = math.min(ls, le), math.max(ls, le)
+  cs, ce = math.min(cs, ce), math.max(cs, ce)
+
   return vim.api.nvim_buf_get_text(0, ls - 1, cs - 1, le - 1, ce, {})
 end
 
