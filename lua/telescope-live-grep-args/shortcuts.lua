e33b30f002a64366b62e37c0f1eb18ce223754da
llmchild <2803354011@qq.com>
2024-07-26 14:54:51 +0800

    switch interface to quickly toggle current buffer search

--- lua/telescope-live-grep-args/shortcuts.lua
+++ lua/telescope-live-grep-args/shortcuts.lua
@@ -60,7 +60,7 @@ M.grep_visual_selection = function(opts)
   live_grep_args.live_grep_args(opts)
 end
 
-M.grep_word_visual_selection_current_buffer = function(opts)
+M.grep_visual_selection_current_buffer = function(opts)
   opts = opts or {}
   local curr_path = vim.fn.expand("%")
   opts["search_dirs"] = { curr_path }
@@ -73,5 +73,36 @@ M.grep_word_under_cursor_current_buffer = function(opts)
   opts["search_dirs"] = { curr_path }
   M.grep_word_under_cursor(opts)
 end
+local function search_switch(user_func)
+    local function current_buffer_live_grep_args(_opts, is_current_buffer)
+      _opts = _opts or {}
+      _opts.attach_mappings = function(_, _map)
+          _map({ "n", "i" }, "<C-a>", function(prompt_bufnr) -- <C-h> to toggle modes
+            local prompt = require("telescope.actions.state").get_current_line()
+            require("telescope.actions").close(prompt_bufnr)
+            is_current_buffer = not is_current_buffer
+            current_buffer_live_grep_args({ default_text = prompt}, is_current_buffer)
+          end)
+          return true
+      end
+      if is_current_buffer then
+          _opts.prompt_title = "Live Grep Args (Current Buffer)"
+          local curr_path = vim.fn.expand("%")
+          _opts["search_dirs"] = { curr_path }
+          live_grep_args.live_grep_args(_opts)
+      else
+          _opts.prompt_title = "Live Grep (Args)"
+          user_func(_opts)
+          -- switch back do not need word/visual selection shortcut again
+          user_func = live_grep_args.live_grep_args
+      end
+    end
+  return current_buffer_live_grep_args
+end
+local normal_grep  = M.live_grep_args_shortcuts.grep_word_under_cursor
+local v_normal_grep = M.live_grep_args_shortcuts.grep_visual_selection
+-- now you can use <C-a> to ennable powerful switch bewteen current buffer and normal grep
+M.grep_word_under_cursor_switch = search_switch(normal_grep)
+M.grep_word_visual_selection_switch = search_switch(v_normal_grep)
 
 return M
